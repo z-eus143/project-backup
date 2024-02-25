f284af22676a0f7321222967124917a48db37f39
z-eus143 <tmaji435@gmail.com>
2024-02-25 19:11:49 +0530

    s-c

--- client/src/components/Wishlist/wishlist.jsx
+++ client/src/components/Wishlist/wishlist.jsx
@@ -30,15 +30,15 @@ useEffect(() => {
     <LoadingBar color='#f11946' progress={progress} onLoaderFinished={() => setProgress(0)}/>
     <Header />
     <div className="property">{
-        propertydata.map((item, index) => {
-            // return(<h1 key={index}>Type: {item.Type}, BedRooms: {item.NoBedRoom}, Amenities: {item.Amenities}</h1>)
-            return(<div key={index}> 
-                <h1 className="data">Type: {item.Type}</h1>
-                <h1 className="data">BedRooms: {item.NoBedRoom}</h1>
-                <h1 className="data">Amenities: {item.Amenities}</h1>
-                <h1 className="data">Description : {item.Description}</h1>
-            </div>)
-        })
+        // propertydata.map((item, index) => {
+        //     // return(<h1 key={index}>Type: {item.Type}, BedRooms: {item.NoBedRoom}, Amenities: {item.Amenities}</h1>)
+        //     return(<div key={index}> 
+        //         <h1 className="data">Type: {item.Type}</h1>
+        //         <h1 className="data">BedRooms: {item.NoBedRoom}</h1>
+        //         <h1 className="data">Amenities: {item.Amenities}</h1>
+        //         <h1 className="data">Description : {item.Description}</h1>
+        //     </div>)
+        // })
     }</div>
     </>
 )}
