a6077201c7fb99f2161c1774c43d5e426970945e
z-eus143 <tmaji435@gmail.com>
2024-03-01 10:39:20 +0530

    -

--- client/src/components/createPost/formcomponent/type.jsx
+++ client/src/components/createPost/formcomponent/type.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 
 // property listing progress
 export const Propertydescribe = (props) => {
@@ -6,8 +8,9 @@ export const Propertydescribe = (props) => {
         <h1 className="room_form">Describe Property</h1>
         {(props.type == "Room") 
         ? <><h1>{props.type}</h1> <Propertyroom/></> 
-        : (props.type == "Sharedroom") ? <h1>{props.type}</h1> 
-        : <h1>{props.type}</h1>}
+        : (props.type == "Sharedroom") 
+        ? <><h1>{props.type}</h1> <Sharedproperty/></>
+        : <><h1>{props.type}</h1></>}
         </>
     )
 }
@@ -34,6 +37,7 @@ export const Images = () => {
     return(
         <>
         <h1 className="room_form">Images</h1>
+        <Imagemulupload/>
         </>
     )
 }
@@ -44,38 +48,38 @@ export const Images = () => {
 // for room
 const Propertyroom = () => {
     return( <>
-     <form>
-         <div>
-             <label>Property Name</label>
-             <input type="text"/>
+     <form className="for_in_data">
+         <div className="for_div">
+             <label className="for_lab">Property Name</label>
+             <input type="text" className="in_txt"/>
          </div>
-         <div>
-            <label>Number of Bedrooms: </label>
-            <input type="Number" min={1} max={10}/>
+         <div className="for_div">
+            <label className="for_lab">Number of Bedrooms: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
          </div>
-         <div>
-            <label>Number of Bathrooms: </label>
-            <input type="Number" min={1} max={10}/>
+         <div className="for_div">
+            <label className="for_lab">Number of Bathrooms: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
          </div>
-         <div>
-            <label>Maximum Occupancy: </label>
-            <input type="Number" min={1} max={10}/>
+         <div className="for_div">
+            <label className="for_lab">Maximum Occupancy: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
          </div>
-         <div>
-            <label>Description: </label>
-            <textarea rows="2" cols="20" />
+         <div className="for_div">
+            <label className="for_lab">Description: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
          </div>
-         <div>
-            <label>Amenities: </label>
-            <textarea rows="2" cols="20"/>
+         <div className="for_div">
+            <label className="for_lab">Amenities: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
          </div>
-         <div>
-            <label>House Rules: </label>
-            <textarea rows="2" cols="20"/>
+         <div className="for_div">
+            <label className="for_lab">House Rules: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
          </div>
-         <div>
-            <label>Additional info: </label>
-            <textarea rows="2" cols="20"/>
+         <div className="for_div">
+            <label className="for_lab">Additional info: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
          </div>
      </form>
      </>)
@@ -84,39 +88,126 @@ const Propertyroom = () => {
 
 // for shared room
 
+const Sharedproperty = () => {
+    return(<>
+    <form className="for_in_data">
+         <div className="for_div">
+             <label className="for_lab">Property Name</label>
+             <input type="text" className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Number of Bedrooms: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Number of Bathrooms: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Maximum Occupancy: </label>
+            <input type="Number" min={1} max={10} className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Description: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Amenities: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">House Rules: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
+         </div>
+         <div className="for_div">
+            <label className="for_lab">Additional info: </label>
+            <textarea rows="1" cols="20" className="in_txt"/>
+         </div>
+     </form></>)
+}    
 
 // Property Address form
 
 const Roomaddress = () => {
     return (
         <>
-        <form>
-            <div>
-                <label>Property Name / Flat No: </label>
-                <input type="text"/>
+        <form className="for_in_data">
+            <div className="for_div">
+                <label className="for_lab">Property Name / Flat No: </label>
+                <input type="text" className="in_txt"/>
             </div>
-            <div>
-                <label>Street Address: </label>
-                <input type="text"/>
+            <div className="for_div">
+                <label className="for_lab">Street Address: </label>
+                <input type="text" className="in_txt"/>
             </div>
-            <div>
-                <label>Locatity: </label>
-                <input type="text"/>
+            <div className="for_div">
+                <label className="for_lab">Locatity: </label>
+                <input type="text" className="in_txt"/>
             </div>
-            <div>
-                <label>City: </label>
-                <input type="text"/>
+            <div className="for_div">
+                <label className="for_lab">City: </label>
+                <input type="text" className="in_txt"/>
             </div>
-            <div>
-                <label>State/Provience: </label>
-                <input type="text"/>
+            <div className="for_div">
+                <label className="for_lab">State/Provience: </label>
+                <input type="text" className="in_txt"/>
             </div>
-            <div>
-                <label>Area Pincode: </label>
-                <input type="text"/>
+            <div className="for_div">
+                <label className="for_lab">Area Pincode: </label>
+                <input type="text" className="in_txt"/>
             </div>
         </form>
         </>
     )
 }
 
+// images
+
+const Imagemulupload = () => {
+    const [images, setImages] = useState([]);
+
+    const handleImageChange = (e) => {
+      const selectedFile = e.target.files[0];
+      if (images.length < 5 && selectedFile && selectedFile.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          setImages([...images, event.target.result]);
+        };
+        reader.readAsDataURL(selectedFile);
+      }
+    };
+  
+    return (
+      <div>
+        <input
+          type="file"
+          onChange={handleImageChange}
+          disabled={images.length >= 5}
+        />
+        <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: '10px' , justifyContent: 'center'}}>
+          {images.map((image, index) => (
+            <div key={index} style={{ margin: '5px' }}>
+              <UploadedImage image={image} />
+            </div>
+          ))}
+          {images.length < 5 && <PlaceholderImage />}
+        </div>
+      </div>
+    );
+  }
+  
+  function PlaceholderImage() {
+    return (
+      <div style={{ margin: '5px', width: '180px', height: '150px', border: '1px solid #ccc', display: 'flex', justifyContent: 'center', alignItems: 'center', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+        <span>Preview</span>
+      </div>
+    );
+  }
+  
+  function UploadedImage({ image }) {
+    return (
+      <div style={{ margin: '5px', padding : '5px', borderRadius: '5px', overflow: 'hidden', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+        <img src={image} alt="Uploaded" style={{ maxWidth: '200px', maxHeight: '200px', aspectRatio: '3/2', objectFit: 'contain' }} />
+      </div>
+    );
+  }
